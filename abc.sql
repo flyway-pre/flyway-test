9f62dfd6bf18e8d95a0a7b01ea9442830c23ed6d
jaehwankim1 <jaehwankim1@naver.com>
2019-05-29 14:50:02 +0900

    insert query(about 3 person) for abc.sql by jaehwankim1

---
+++ abc.sql
@@ -0,0 +1,5 @@
+INSERT INTO person(ID, NAME, EMAIL, PHONE)
+VALUES 
+(1, 'KIM', 'XXX@hanmail.kr', '010-1234-5678'),
+(2, 'LEE', 'ZBS@naver.kr', '010-2732-6653'),
+(3, 'PARK', 'QEW@naver.kr', '010-6176-2354')
